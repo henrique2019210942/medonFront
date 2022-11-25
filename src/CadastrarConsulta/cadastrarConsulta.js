3e10f7b7bd2b2a7de3b41bdcb7c1f7b54226ded7
Henrique Lira Da Silva <henrique.2019210942@unicap.br>
2022-11-25 16:38:05 -0300

    https

--- src/CadastrarConsulta/cadastrarConsulta.js
+++ src/CadastrarConsulta/cadastrarConsulta.js
@@ -12,7 +12,7 @@ function App() {
   const [listaDeConsultasM, setListaDeConsultasM] = useState([]);
 
    useEffect(() => {
-    Axios.get("http://medonback.herokuapp.com/cadastrarConsulta/").then(
+    Axios.get("https://medonback.herokuapp.com/cadastrarConsulta/").then(
       (response) => {
         setListaDeConsultasM(response.data);
       }
@@ -20,7 +20,7 @@ function App() {
   }, []);
 
   const cadastrarConsulta = () => {
-    Axios.post("http://medonback.herokuapp.com/cadastrarConsulta/", {
+    Axios.post("https://medonback.herokuapp.com/cadastrarConsulta/", {
       tipo: tipo,
       endereco: endereco,
       data: data,
